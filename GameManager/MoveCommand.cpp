242d57bfdf4ccd414eaabe4d98c08a4f7cba323c
BraydenJamesE <95278801+BraydenJamesE@users.noreply.github.com>
2023-03-28 15:28:04 -0700

    Updates made to MoveCommand to ensure player cannot input invalid move.

--- GameManager/MoveCommand.cpp
+++ GameManager/MoveCommand.cpp
@@ -9,15 +9,20 @@
 namespace Game {
     void MoveCommand::execute(Game::GameManager *gm) {
         const string delimiter = " ";
-        std::cout << "Moving" << std::endl;
+        std::string invalidMoveMessage = "An invalid move hath been made, 'tis clear,\n"
+                                         "Pray try again with care and thou shalt hear.\n";
         std::string x = params.substr(0, params.find(delimiter));
         std::string y = params.substr(params.find(delimiter) + 1, params.length() - 1);
         int xInt = std::stoi(x) - 1;
         int yInt = std::stoi(y) - 1;
         bool adjacentCell = false;
+
         if(xInt >= 0 && xInt < gm->GetMap()->GetX() && yInt >= 0 && yInt < gm->GetMap()->GetY()) {
             if(abs((xInt - gm->playerCharacter->cellx) + (yInt - gm->playerCharacter->celly)) == 1){
                 adjacentCell = true;
+            }
+            else {
+                std::cout << invalidMoveMessage;
             }
 
             if(adjacentCell == true && gm->GetMap()->GetCell(xInt, yInt)->characterCount == 0){
@@ -26,8 +31,7 @@ namespace Game {
             }
         }
         else {
-            std::cout << "An invalid move hath been made, 'tis clear,\n"
-                         "Pray try again with care and thou shalt hear.\n";
+            std::cout << invalidMoveMessage;
         }
     } // end of "MoveCommand" function
 } // Game
