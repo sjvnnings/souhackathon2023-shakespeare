94634909d8fa04e6c37ccc3e3f3ed41fdc702974
BraydenJamesE <95278801+BraydenJamesE@users.noreply.github.com>
2023-03-28 15:23:19 -0700

    Updates made to MoveCommand to ensure player cannot input invalid move.

--- GameManager/MoveCommand.cpp
+++ GameManager/MoveCommand.cpp
@@ -15,14 +15,19 @@ namespace Game {
         int xInt = std::stoi(x) - 1;
         int yInt = std::stoi(y) - 1;
         bool adjacentCell = false;
+        if(xInt >= 0 && xInt < gm->GetMap()->GetX() && yInt >= 0 && yInt < gm->GetMap()->GetY()) {
+            if(abs((xInt - gm->playerCharacter->cellx) + (yInt - gm->playerCharacter->celly)) == 1){
+                adjacentCell = true;
+            }
 
-        if(abs((xInt - gm->playerCharacter->cellx) + (yInt - gm->playerCharacter->celly)) == 1){
-            adjacentCell = true;
+            if(adjacentCell == true && gm->GetMap()->GetCell(xInt, yInt)->characterCount == 0){
+                gm->GetMap()->GetCell(gm->playerCharacter->cellx, gm->playerCharacter->celly)->RemoveCharacter(gm->playerCharacter);
+                gm->GetMap()->GetCell(xInt, yInt)->AddCharacter(gm->playerCharacter);
+            }
         }
-
-        if(adjacentCell == true && gm->GetMap()->GetCell(xInt, yInt)->characterCount == 0){
-            gm->GetMap()->GetCell(gm->playerCharacter->cellx, gm->playerCharacter->celly)->RemoveCharacter(gm->playerCharacter);
-            gm->GetMap()->GetCell(xInt, yInt)->AddCharacter(gm->playerCharacter);
+        else {
+            std::cout << "An invalid move hath been made, 'tis clear,\n"
+                         "Pray try again with care and thou shalt hear.\n";
         }
     } // end of "MoveCommand" function
 } // Game
